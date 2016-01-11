40f5a7cde5e273c348afbce13439a4ae3c7dff2f
Martin Unzner <martin.unzner@citrix.com>
2016-01-11 20:20:55 +0100

    Removed YCM in favour of eclim

--- pullall.sh
+++ pullall.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 git fetch --all --prune
 git pull --rebase && \
-    git submodule update --init --recursive && \
-    vim/bundle/build-ycm-modules.sh
+    git submodule update --init --recursive
 
